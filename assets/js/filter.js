ad8a1e43667aaa74510dbb96d82abf33de9511a3
Ciobotaru Stefan <stfn_ciobotaru@yahoo.com>
2018-05-07 23:02:46 +0300

    Add Javascript alerts

---
+++ assets/js/filter.js
@@ -0,0 +1,11 @@
+function onPriceOrder() {
+    alert("click pe pret");
+}
+
+function onDiscountOrder() {
+    alert("click pe discount");
+}
+
+function onReviewOrder() {
+    alert("click pe review");
+}
